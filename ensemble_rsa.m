3f08fc5bd36ffebe4840513c37ed1ede6d815f01
Greta Vilidaite <vilidaite@gmail.com>
2018-08-30 13:35:46 -0700

    Fixed indexing when subjects have different numbers of trials; made more sensible file load GUI

--- ensemble_rsa.m
+++ ensemble_rsa.m
@@ -1,4 +1,4 @@
-% Ensemble RSA analysis, pretty much copied from gv_kaneshiro_rsa.m with an
+% Ensemble RSA analysis, pretty much copied from kaneshiro_rsa.m with an
 % added bit at the start that combines subject data into a matrix
 % GV 18th May 2018
 
@@ -17,13 +17,13 @@ close all
 clear all
 
 % SOME FLAGS TO SET:
-doRSA = 1; % 1 = do RSA using the following flags; 0 = just load in the specified file and plot stuff
+doRSA = 0; % 1 = do RSA using the following flags; 0 = just load in the specified file and plot stuff
 
 
 
 % Add any other information here (e.g. if you used some averaging
 % procedures, artefact rejection, etc):
-RSAinfo.other = '';
+RSAinfo.other = 'RSA after index bug fix. Averaging over 3. Percentages included';
 
 
 outputEnd = 'RSAensemble'; % leave as is
@@ -32,15 +32,15 @@ outputEnd = 'RSAensemble'; % leave as is
 % plot data, completing the folder name using the below flags (the folder name is
 % auto-completed based on them)
 
-averagetrials = -1; % how many trials to average over during classifyEEG. Set to negative numebr to not do averaging
+averagetrials = 3; % how many trials to average over during classifyEEG. Set to negative numebr to not do averaging
 
-whichlabels = 1; % 1 = object category labels; 2 = exemplar labels
+whichlabels = 2; % 1 = object category labels; 2 = exemplar labels
 
-suppress_diag = 0; % 1 = suppress the diagonale in the CM and distances matrices when using exemplar labels. Doesn't do anything in other plots/analyses 
+suppress_diag = 1; % 1 = suppress the diagonale in the CM and distances matrices when using exemplar labels. Doesn't do anything in other plots/analyses 
 
 timepoints = 1;
 % 1 = all timepoints and all electrodes;
-% 2 = in moving windows (number of which specified below). Time windows are separated using gv_make_sliding_windows.m
+% 2 = in moving windows (number of which specified below). Time windows are separated using make_sliding_windows.m
 
 
 NTimeWindows = 10; % number of time windows to separate the data into if
@@ -64,7 +64,7 @@ if (timepoints == 1)
     RSAinfo.timepoins = 'all timepoints';
 elseif (timepoints == 2)
     namestr2 = strcat('_windowsof', num2str(NTimeWindows));
-    RSAinfo.timepoins = 'Sliding time windows, separated by gv_make_sliding_windows';
+    RSAinfo.timepoins = 'Sliding time windows, separated by make_sliding_windows';
 end
 
 outputFolder = strcat(outputEnd, namestr1, namestr2,'/'); % renaming the folder properly
@@ -72,21 +72,30 @@ outputFolder = strcat(outputEnd, namestr1, namestr2,'/'); % renaming the folder 
 
 % RSA analysis ------------------------------------------------------------
 if doRSA == 1 % if flag set to 1, do the following RSA analysis and save the results into a .mat file
- 
-        
-    % this opens a GUI to add files for analysis:
-    file = 1;
-    count = 1;
-    while any(file)
-        [file path] = uigetfile('', 'Choose _data4RSA.mat files to do RSA on. CLick CANCEL when finished');
-        fileList{count} = file;
-        count = count + 1;
-    end
-    
-    datapath = uigetdir('Point to the directory where you want the RSA output folder to be')
 
-    if ~exist(strcat(datapath,outputFolder)) % if output folder for that type of analysis does not exist, create it
-        mkdir(outputFolder)
+    str ={'Process selected files (select files one by one)','Process all files in a folder (select folder)'}
+    selectMode = listdlg('PromptString','Select a file:','SelectionMode','single','ListString',str)
+    if selectMode==1
+        % this opens a GUI to add files for analysis one by one:
+
+        [fileList dataPath] = uigetfile('', 'Choose _data4RSA.mat files to do RSA on.','Multiselect','on');
+
+    else
+        dataPath = uigetdir('Choose folder containing _data4RSA.mat files');
+        inputfiles = dir(strcat(dataPath,'/*_data4RSA.mat'));
+        
+        for f = 1:length(inputfiles)
+            fileList{f} = inputfiles(f).name;
+        end
+
+    end
+        
+
+    
+    outputPath = uigetdir('Point to the directory where you want the RSA output folder to be')
+strcat(outputPath,'/',outputFolder)
+    if ~exist(strcat(outputPath,'/',outputFolder)) % if output folder for that type of analysis does not exist, create it
+        mkdir(outputPath, outputFolder)
     end
 
     % setting up some empty cell arrays to store data in:
@@ -100,7 +109,7 @@ if doRSA == 1 % if flag set to 1, do the following RSA analysis and save the res
         outputFileNameStart = currentfilename(1:(end-13)); % this parses the filename string and only keeps the subject number/initials
         disp(strcat('##### Classifying EEG data for ', currentfilename,' #####'))
         
-        currentfile = load(strcat(datapath,'/',currentfilename)); % category labels (as opposed to
+        currentfile = load(strcat(dataPath,'/',currentfilename)); % category labels (as opposed to
 
         if whichlabels == 1 % object category classification
            currentY = currentfile.labelsByCond; % all category labels in a single vector
@@ -115,9 +124,12 @@ if doRSA == 1 % if flag set to 1, do the following RSA analysis and save the res
             currentX = currentfile.EEGdata_downsampled;
 
             [CM, accuracy, predY{n}, currentpVal{n}, classifierInfo{n}] = classifyEEG(currentX, currentY, 'classify','LDA','averageTrials',averagetrials);
-
+            totalObs = sum(CM,2); % calculating total number of observations (trials) in each row
+            for col = 1:size(CM,2)
+                CM_percent(:,col) = CM(:,col).*100./totalObs(col);
+            end
         elseif (timepoints == 2)
-            [windowedData indexes] = gv_make_sliding_windows(currentfile.EEGdata_downsampled, NTimeWindows, 1);
+            [windowedData indexes] = make_sliding_windows(currentfile.EEGdata_downsampled, NTimeWindows, 1);
             windowsforplotting = indexes; % for saving
             for t = 1:NTimeWindows 
                 
@@ -132,16 +144,21 @@ if doRSA == 1 % if flag set to 1, do the following RSA analysis and save the res
 
 
         if (timepoints == 1)
-            save(strcat(datapath, outputFolder,outputFileNameStart,'.mat'),'CM','accuracy','predY','pVal','classifierInfo','RSAinfo');
+            save(strcat(outputPath,'/', outputFolder, outputFileNameStart,'.mat'),'CM','CM_percent','accuracy','predY','pVal','classifierInfo','RSAinfo');
         else
-            save(strcat(datapath, outputFolder,outputFileNameStart,'.mat'),'CM','accuracy','predY','pVal','classifierInfo','RSAinfo','windowsforplotting');
+            save(strcat(outputPath, '/', outputFolder, outputFileNameStart,'.mat'),'CM','accuracy','predY','pVal','classifierInfo','RSAinfo','windowsforplotting');
         end
     end
     
     for n = 1:(length(fileList)-1) % go  through fileList but ignore the last cell because it's always just a zero
         currentfilename = fileList{n};
         % load in the files
-        load(strcat(datapath, outputFolder,outputFileNameStart,'.mat'))
+        load(strcat(outputPath, '/', outputFolder,outputFileNameStart,'.mat'))
+        
+        if exist('CM_percent')
+           CM = CM_percent; % if CM_percent exists, use that as CM instead 
+        end
+        
         if (timepoints == 1)
             allCM(n,:,:) = CM;
             allAccuracy(n) = accuracy;
@@ -152,9 +169,9 @@ if doRSA == 1 % if flag set to 1, do the following RSA analysis and save the res
     end
 
 else % if doRSA is NOT 1
-    datapath = uigetdir('Point to the folder CONTAINING the RSA output folders');
+    outputPath = uigetdir('','Point to the folder CONTAINING the RSA output FOLDERS');
     
-    RSApath = strcat(datapath,'/',outputFolder);
+    RSApath = strcat(outputPath,'/',outputFolder);
     
     RSAfiles = dir(strcat(RSApath,'*.mat'))
     
@@ -174,7 +191,7 @@ end
 % Plotting the confusion matrices -----------------------------------------
 
 grandCM = squeeze(mean(allCM,1)); % mean over subjects
-grandCM = grandCM;
+grandCM = grandCM./100;
 
 if whichlabels == 1
     axislabels = {'HB', 'HF', 'AB', 'AF', 'IN', 'IA'};
@@ -187,8 +204,8 @@ end
 figno = 0;
 
 if (timepoints == 1) % if all timepoints analysed together
-    RDM = computeRDM(grandCM);
-    
+    RDM = computeRDM(grandCM, 'normalize','none','symmetrize','none');
+   
     if (whichlabels == 2)&&(suppress_diag == 1)
         grandCM = grandCM - diag(diag(grandCM)); % a fudge to replace the diagonale with zeros if looking at exemplar level classification
     end
@@ -200,6 +217,7 @@ if (timepoints == 1) % if all timepoints analysed together
     plotMatrix(grandCM, 'axisLabels', axislabels, 'colorMap', 'jet', 'colorBar', 1, 'matrixLabels', 0)
     % Enlarge figure to full screen.
     set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]);
+    
     
     figno = figno+1;
     figure(figno)
--- prepare_ensemble_data.m
+++ prepare_ensemble_data.m
@@ -11,21 +11,23 @@
 close all
 clear all
 
-% update this with the subject numbers (names of the folders that data is
-% stored in
-subjects = {'ASG1','ASG2','AY1','AY2','nl-0043_p1','nl-0045_p1','nl-0046_p1','nl-0047_p1','nl-0048_p1','PJK1','PJK2','VV1','VV2','WM','WM2'};
+%subjects = {'ASG1','ASG2','AY1','AY2','nl-0043_p1','nl-0045_p1','nl-0046_p1','nl-0047_p1','nl-0048_p1','PJK1','PJK2','VV1','VV2','WM'};
 
-EEGpath = '/Volumes/Denali_4D2/2015_Ensembles/Ensembles_MATLAB/'; % point this to the directory containing individual subject folders (on Denali_4D2)
-outputpath = '~/Greta/2015_Ensembles_data/'; % point this to where you want the prepared files saved - usually a local directory
+% 'nl-0014','nl-0034','nl-0043','nl-0045','nl-0046','nl-0047','nl-0048','nl-1671','nl-1672',
 
+subjects = {'nl-1676','nl-1677','nl-1678','nl-1681','nl-1684','nl-1685','nl-1686'};
 
-downsampling_factor = 4; % this downsamples EEG data to make it more manageable
+% EEGpath = '~/Greta/2015_Ensembles_data/EEG_files/';
+EEGpath = '~/Greta/2018_Ensemble_greyscale_data/EEG_files/';
+
+cd(EEGpath);
+
+downsampling_factor = 4;
 
 % find and open all Run Time Segment files and put them in one matrix:
 
 for s = 1:length(subjects)
-    
-    cd(EEGpath)
+    s
     allSeg = dir(strcat(subjects{s},'/RTSeg*')); % get info (names) of all segment files
 
     count = 0;
@@ -37,18 +39,19 @@ for s = 1:length(subjects)
 
         if ~isempty(TimeLine) % checks that there is some information in there
             count = count + 1;
-
             % adding each structure's info onto the end of our vectors
-            ind_start = count * length(TimeLine) - length(TimeLine) + 1;
-            ind_end = count * length(TimeLine);
+            if count == 1
+                ind_start = 1;
+                ind_end = length(TimeLine);
+            else
+                ind_start = ind_end + 1;
+                ind_end = length(TimeLine) + ind_start - 1;
+            end
 
             currentSeg = extractfield(TimeLine, 'cndNmb');
             condNs(ind_start:ind_end) = currentSeg;
-
             currentSeg = extractfield(TimeLine, 'trlNmb');
             trialNs(ind_start:ind_end) = currentSeg;
-
-
         end
     end
 
@@ -60,10 +63,16 @@ for s = 1:length(subjects)
     count = 0;
     for c = 1:6 % for each condition
 
-        curExemps = exempcodeSeg((condNs==c)); % retrieving unique exemplar numebrs just for this condition
+        curExemps = exempcodeSeg((condNs==c)); % retrieving unique exemplar numbers just for this condition
+        c
+        exempcodeSeg(1:20)
+        condNs(1:20)
+        curExemps(1:20)
+        clear allRaw
         allRaw = dir(strcat(subjects{s},'/Raw_c00',num2str(c),'_t*')); % get info (names) of all segment files
-        for t = 1:length(allRaw)
 
+        for t = 1:length(allRaw)
+            
             count = count + 1;
 
             EEG = load(strcat(subjects{s},'/','Raw_c',num2str(c,'%03d'),'_t',num2str(t,'%03d'),'.mat')); % loading the file
@@ -83,6 +92,6 @@ for s = 1:length(subjects)
         end
     end
 
-    save(strcat(outputpath,subjects{s},'_data4RSA.mat'), 'EEGdata', 'labelsByCond', 'labelsByExemp', 'EEGdata_downsampled')
+    save(strcat('~/Greta/2018_Ensemble_greyscale_data/Matclass_ready_08_2018/',subjects{s},'_data4RSA.mat'), 'EEGdata', 'labelsByCond', 'labelsByExemp', 'EEGdata_downsampled')
 end
 
